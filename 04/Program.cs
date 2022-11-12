6ec03330925557a11a6f3641b8997c850dd50ad8
Kellamon <fqvxiwzz5iym@mail.ru>
2022-11-12 18:48:24 +0300

    Сделаны 3 домашние задачи

---
+++ 04/Program.cs
@@ -0,0 +1,13 @@
+﻿// 4. По заданному с клавиатуры номеру дня недели вывести его название
+//Есть конечно более простые и более универсальные варианты, но мы их не изучали
+
+System.Console.WriteLine("Введите число от 1 до 7");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a==1) System.Console.WriteLine("Сегондя Понедельник");
+if (a==2) System.Console.WriteLine("Сегондя Вторник");
+if (a==3) System.Console.WriteLine("Сегондя Среда");
+if (a==4) System.Console.WriteLine("Сегондя Четверг");
+if (a==5) System.Console.WriteLine("Сегондя Пятница");
+if (a==6) System.Console.WriteLine("Сегондя Суббота");
+if (a==7) System.Console.WriteLine("Сегондя Воскресенье");
+if (a>7) System.Console.WriteLine("Не шали, просили от 1 до 7");
---
+++ 05/Program.cs
@@ -0,0 +1,24 @@
+﻿// 5. С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+int a;
+int b;
+int c;
+int max;
+System.Console.Write("Введите число a= ");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.Write("Введите число b= ");
+string? v=Console.ReadLine();
+b=Convert.ToInt32(v);
+System.Console.Write("Введите число c= ");
+string? j=Console.ReadLine();
+c=Convert.ToInt32(j);
+max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+System.Console.WriteLine(max + "является максимальным");    
+
+
+     
