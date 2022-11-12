6d601529c79dd6c6726f36025c15711762850948
Kellamon <fqvxiwzz5iym@mail.ru>
2022-11-12 14:40:33 +0300

    1й семинар

---
+++ 06/Program.cs
@@ -0,0 +1,6 @@
+﻿//Написать программу вычисления значения функции y = sin(a). (Класс Math).
+//cделанно, закоммить
+System.Console.WriteLine("Введите значение: ");
+double a=Convert.ToDouble(Console.ReadLine());
+double y=Math.Sin(a);
+System.Console.WriteLine("Значение Sin= "+y);
---
+++ 09/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести на экран четные числа от 1 до N
+// работает, можно коммитить
+int N=Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<=N)
+{
+    System.Console.Write($"{i} ");
+    i=i+2;
+}
+System.Console.WriteLine();
+for (int j = 2; j <=N; j=j+2)
+{
+    System.Console.Write($"{j} ");
+}
