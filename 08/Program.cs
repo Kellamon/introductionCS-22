f5433ee0bf46e966b28f88695429f1890e8eb71a
Kellamon <fqvxiwzz5iym@mail.ru>
2022-11-12 20:55:26 +0300

    8 сделана

--- 08/Program.cs
+++ 08/Program.cs
@@ -1,1 +1,17 @@
 ﻿// 8. Вывести на экран числа от -N до N
+System.Console.WriteLine("Введите число -N ");
+int N=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число n ");
+int n=Convert.ToInt32(Console.ReadLine());
+int i=0;
+while (i>=N)
+{
+    System.Console.Write($"{i} ");
+    i=i-1;
+}
+int j = 1;
+while (j<=n)
+{
+    System.Console.Write($"{j} ");
+    j++;
+}
