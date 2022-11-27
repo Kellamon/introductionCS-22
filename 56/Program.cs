653c2bc0c0bc05107a7f8b38888bb347755a97c9
Kellamon <fqvxiwzz5iym@mail.ru>
2022-11-27 16:51:45 +0300

    Почти сделанная ДЗ

---
+++ 56/Program.cs
@@ -0,0 +1,32 @@
+﻿// 56. Написать программу копирования массива
+
+int[] a = RandomIntArray(10, 0, 10);
+PrintTo(a);
+System.Console.WriteLine();
+
+
+int[] SecArr = new int[a.GetUpperBound(0) + 1];
+
+Array.Copy(a, SecArr, a.GetUpperBound(0) + 1);
+
+PrintTo(SecArr);
+
+int[] RandomIntArray(int size = 10, int min = 0, int max = 10)
+{
+    int[] a = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = random.Next(min, max + 1);
+    }
+    return a;
+}
+
+void PrintTo(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        System.Console.Write($"{a[i],5}");
+    }
+}
+
